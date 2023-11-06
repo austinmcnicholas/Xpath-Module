0555f243e75462eef406d25cc27abd1b1282ce2c
Samuel Egan <samuel.egan@mendix.com>
2023-11-06 12:49:33 -0700

    Remove deployment files.

--- App/deployment/native/styles.js
+++
@@ -1,7 +0,0 @@
-import * as style0 from "C:/code/xpathchanges/Xpath-Module/App/themesource/myfirstmodule/native/main";
-import * as style1 from "C:/code/xpathchanges/Xpath-Module/App/themesource/xpath/native/main";
-import * as style2 from "C:/code/xpathchanges/Xpath-Module/App/theme/styles";
-
-import { flatten } from "mendix/native";
-
-module.exports = flatten([style0, style1, style2]);
